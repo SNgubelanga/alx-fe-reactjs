92501a7daac351c293102ba658eae40d6b87d491
Sbahle Ngubelanga <sbahle.mkhetheka99@gmail.com>
2025-02-20 22:31:29 +0200

    Modified according to task 3 requirements

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage.jsx';
 import Header from './components/Header.jsx';
 import MainContent from './components/MainContent.jsx';
 import Footer from './components/Footer.jsx';
+import UserProfile from './components/UserProfile.jsx';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -21,6 +22,7 @@ function App() {
       </div>
       <Header />
       <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <MainContent />
       <h1>Vite + React</h1>
       <div className="card">
@@ -39,4 +41,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
