f4b88f374acec9bbb10295876a938d3f4692337f
Sbahle Ngubelanga <sbahle.mkhetheka99@gmail.com>
2025-03-01 11:34:36 +0200

    added alx-react-app-new

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer  style={{borderTop:"1px solid #c9c9c9", marginTop:"10px"}}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main
+      style={{ background: "#F7F7F7", padding: "10px", borderRadius: "4px" }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
