c8a68346baba3786b296a07d3d18e93e3451668b
Sbahle Ngubelanga <sbahle.mkhetheka99@gmail.com>
2025-03-02 08:25:21 +0200

    Moved userDetails to components

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
 import ProfilePage from './components/ProfilePage.jsx';
-import UserContext from './UserContext.js';
+import UserContext from './components/UserContext.js';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
