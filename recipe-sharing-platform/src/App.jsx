1294e96815039f422c992a705698969eade16c48
Sbahle Ngubelanga <sbahle.mkhetheka99@gmail.com>
2025-03-22 15:27:04 +0200

    Added user interactivity to my recipe sharing platform

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { HomePage } from './components/HomePage';
 import { RecipeDetail } from './components/RecipeDetail';
 import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import { AddRecipeForm } from './components/AddRecipeForm';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,12 +15,13 @@ function App() {
     <h2 className='mb-3 uppercase'>Recipe Sharing Platform</h2>
     <hr className='mb-3'/>
     <div>
-      {/* <Link to={"/"}>Home</Link> */}
-      {/* <Link to={"/recipe"}>Home</Link> */}
+      {/* <Link to={"/"}>Home</Link>
+      <Link to={"/new-recipe"}>New Recipe</Link> */}
     </div>
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/new-recipe" element={<AddRecipeForm />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
       </Routes>
     </Router>
---
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+
+export const AddRecipeForm = () => {
+  const [title, setTitle] = useState("");
+  const [instructions, setInstructions] = useState("");
+  const [ingredients, setIngredients] = useState("");
+
+  // Errors
+  const [ingredientsError, setingredientsError] = useState("");
+  const [titleError, setTitleError] = useState("");
+  const [instructionError, setinstructionError] = useState("");
+
+  // Submit Form
+  const validateFields = (e) => {
+    e.preventDefault();
+
+    if (ingredients.trim() === "") {
+      setingredientsError("Ingredients field is required.");
+    }
+    if (instructions.trim() === "") {
+      setinstructionError("Instruction field is required.");
+    }
+    if (title.trim() === "") {
+      setTitleError("Title field is required.");}
+
+    if(title.trim() && ingredients.trim() && instructions.trim()){
+      alert("Validated.")
+    }
+  };
+
+  return (
+    <>
+      <div className="w-full">
+        <form onSubmit={validateFields}>
+          <div className="mb-5">
+            <label htmlFor="title">
+              Title <span className="text-red-600">*</span>
+            </label>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              name="title"
+              onFocus={(e) => setTitleError("")}
+              id="title"
+              className="shadow appearance-none bg-slate-100 rounded w-full py-2 px-3 text-gray-900 leading-tight focus:outline focus:shadow-outline"
+              placeholder="Pasta"
+            />
+            <small className="text-red-600">
+              <i>{titleError}</i>
+            </small>
+          </div>
+          <div className="mb-5">
+            <label htmlFor="image">
+              Image <span className="text-red-600">*</span>
+            </label>
+            <input
+              type="file"
+              name="image"
+              id="image"
+              className="shadow appearance-none bg-slate-100 rounded w-full py-2 px-3 text-gray-900 leading-tight focus:outline focus:shadow-outline"
+              placeholder="Pasta"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="ingredients">
+              Ingredients <span className="text-red-600">*</span>
+            </label>
+            <textarea
+              name="ingredients"
+              onChange={(e) => setIngredients(e.target.value)}
+              value={ingredients}
+              id="ingredients"
+              onFocus={(e) => setingredientsError("")}
+              placeholder="Write ingredients here.."
+              className="shadow appearance-none bg-slate-100 rounded w-full py-2 px-3 text-gray-900"
+            ></textarea>
+            <small className="text-red-600">
+              <i>{ingredientsError}</i>
+            </small>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="instructions">
+              Instructions <span className="text-red-600">*</span>
+            </label>
+            <textarea
+              name="instructions"
+              id="instructions"
+              value={instructions}
+              onFocus={(e) => setinstructionError("")}
+              onChange={(e) => setInstructions(e.target.value)}
+              placeholder="Write instructions here.."
+              className="shadow appearance-none bg-slate-100 rounded w-full py-2 px-3 text-gray-900"
+            ></textarea>
+            <small className="text-red-600">
+              <i>{instructionError}</i>
+            </small>
+          </div>
+          <div>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Add Recipe
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
