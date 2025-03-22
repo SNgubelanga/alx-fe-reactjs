4d10f9128e3be5ba2197f0c9f75f1630c7102b31
Sbahle Ngubelanga <sbahle.mkhetheka99@gmail.com>
2025-03-22 09:11:34 +0200

    Modified tailwind.cofig.js according to checker requirements

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,11 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
+  variants: { extend: {} },
   plugins: [],
-}
+};
 
