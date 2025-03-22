74c3cf57922f7cab627cba24b1b981acc0371bec
Sbahle Ngubelanga <sbahle.mkhetheka99@gmail.com>
2025-03-22 12:27:31 +0200

    Modified according to checker req

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -16,7 +16,7 @@ export const HomePage = () => {
     return(
         
         <>
-        <div className="grid grid-cols-3 md:gap-4 sm:grid-cols-1 sm:gap-3 rounded-sm">
+        <div className="grid grid-cols-3 md:grid-cols-2 md:gap-4 sm:grid-cols-1 sm:gap-3 rounded-sm">
         {recipes.map(recipe =>  
         <div key={recipe.id} className="bg-stone-300 border-2 hover:border-fuchsia-300 hover:shadow-xl sm:p-5 sm:my-6 md:p-10 md:my-4">
             <img src={recipe.image} alt={recipe.title} className="w-40 hover:scale-110 transition ease-in-out h-40 mx-auto mb-6 rounded-full border-2 border-fuchsia-300" />
