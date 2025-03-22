e98a48d7f7a20b0330847e9e58ccbc97d43f8fc8
Sbahle Ngubelanga <sbahle.mkhetheka99@gmail.com>
2025-03-22 12:21:15 +0200

    Added text on the HomePage

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -20,8 +20,8 @@ export const HomePage = () => {
         {recipes.map(recipe =>  
         <div key={recipe.id} className="bg-stone-300 border-2 hover:border-fuchsia-300 hover:shadow-xl sm:p-5 sm:my-6 md:p-10 md:my-4">
             <img src={recipe.image} alt={recipe.title} className="w-40 hover:scale-110 transition ease-in-out h-40 mx-auto mb-6 rounded-full border-2 border-fuchsia-300" />
-            <h2 className="uppercase">{recipe.title}</h2>
-            <p>{recipe.summary}</p>
+            <h2 className="uppercase sm:text-sm text-center">{recipe.title}</h2>
+            <p className="text-center md:text-base sm:text-lg">{recipe.summary}</p>
         </div>)}
         </div>
         </>
